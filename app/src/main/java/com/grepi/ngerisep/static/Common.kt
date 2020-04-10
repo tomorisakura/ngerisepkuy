20ea4de7acd53936cb740f4dc25a4bca74cef706
tomorisakura <resky67@gmail.com>
2020-04-11 01:12:16 +0700

    fix fetch result

--- app/src/main/java/com/grepi/ngerisep/static/Common.kt
+++ app/src/main/java/com/grepi/ngerisep/static/Common.kt
@@ -1,7 +1,6 @@
 package com.grepi.ngerisep.static
 
 object Common {
-    const val API_KEY = "27788d652f7c45eb9f70d907fc8f829c" // ini sebenare punyanya spooncular wkwkwk
     val url_searchFood = "https://www.themealdb.com/api/json/v1/1/search.php?s="
     var url_seafood = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood"
     var url_dessert = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert"
